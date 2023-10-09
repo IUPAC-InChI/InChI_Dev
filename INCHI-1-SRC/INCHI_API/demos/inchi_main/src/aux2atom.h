ac71becc16e0e454ef75abdfb820c6c7a9d9de81
Djordje Baljozovic <121813753+djb-rwth@users.noreply.github.com>
2023-10-10 00:50:15 +0200

    Version_07_10_2023

--- INCHI-1-SRC/INCHI_API/demos/inchi_main/src/aux2atom.h
+++ INCHI-1-SRC/INCHI_API/demos/inchi_main/src/aux2atom.h
@@ -2,7 +2,7 @@
  * International Chemical Identifier (InChI)
  * Version 1
  * Software version 1.07
- * October 9, 2023
+ * 07/10/2023
  *
  * The InChI library and programs are free software developed under the
  * auspices of the International Union of Pure and Applied Chemistry (IUPAC).
