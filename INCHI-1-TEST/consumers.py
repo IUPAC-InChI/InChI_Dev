0fe47da5d708bc0a3f363957aa38bac742897e66
Jan C. Brammer <jan.c.brammer@gmail.com>
2023-10-16 04:32:33 +0000

    Return message and key from `regression_consumer`

--- INCHI-1-TEST/consumers.py
+++ INCHI-1-TEST/consumers.py
@@ -1,20 +1,21 @@
 import ctypes
 from typing import Callable
 from sdf_pipeline import utils
-from .inchi_api import make_inchi_from_molfile_text
+from .inchi_api import make_inchi_from_molfile_text, get_inchi_key_from_inchi
 from .config import INCHI_API_PARAMETERS
 
 
 def regression_consumer(
     molfile: str, get_molfile_id: Callable, inchi_lib: ctypes.CDLL
 ) -> utils.ConsumerResult:
-    _, inchi_string, _, _, aux_info = make_inchi_from_molfile_text(
+    _, inchi_string, _, message, aux_info = make_inchi_from_molfile_text(
         inchi_lib, molfile, INCHI_API_PARAMETERS
     )
+    _, inchi_key = get_inchi_key_from_inchi(inchi_lib, inchi_string)
 
     return utils.ConsumerResult(
         get_molfile_id(molfile),
         utils.get_current_time(),
         f"consumer: regression; parameters: {INCHI_API_PARAMETERS}",
-        f"{inchi_string}; {aux_info}",
+        f"<inchi>: {inchi_string}\n<aux>: {aux_info}\n<key>: {inchi_key}\n<message>: {message}",
     )
--- INCHI-1-TEST/inchi_api.py
+++ INCHI-1-TEST/inchi_api.py
@@ -1,3 +1,4 @@
+"""`INCHI-1-SRC/INCHI_API/demos/python_sample` stripped to the essentials."""
 import ctypes
 
 
@@ -15,8 +16,6 @@ class InChIOutput(ctypes.Structure):
 def make_inchi_from_molfile_text(
     inchi_lib: ctypes.CDLL, molfile: str, inchi_options: str = ""
 ) -> tuple[int, str, str, str, str]:
-    """`INCHI-1-SRC/INCHI_API/demos/python_sample` stripped to the essentials."""
-
     c_molfile = ctypes.c_char_p(molfile.encode("utf-8"))
     c_inchi_options = ctypes.c_char_p(inchi_options.encode("utf-8"))
 
@@ -26,48 +25,66 @@ def make_inchi_from_molfile_text(
     szLog = ctypes.create_string_buffer(1)
     c_inchi_output = InChIOutput(szInChI, szAuxInfo, szMessage, szLog)
 
-    inchi_exit_code = inchi_lib.MakeINCHIFromMolfileText(
+    exit_code = inchi_lib.MakeINCHIFromMolfileText(
         c_molfile, c_inchi_options, ctypes.byref(c_inchi_output)
     )
 
-    standard_inchi = (
+    inchi = (
         ctypes.cast(c_inchi_output.szInChI, ctypes.c_char_p)
         .value.decode("utf-8")
         .strip()
     )
-    standard_log = (
+    log = (
         ctypes.cast(c_inchi_output.szLog, ctypes.c_char_p).value.decode("utf-8").strip()
     )
-    standard_message = (
+    message = (
         ctypes.cast(c_inchi_output.szMessage, ctypes.c_char_p)
         .value.decode("utf-8")
         .strip()
     )
-    standard_aux = (
+    aux = (
         ctypes.cast(c_inchi_output.szAuxInfo, ctypes.c_char_p)
         .value.decode("utf-8")
         .strip()
     )
 
-    # Deallocate memory reallocated by inchi dll
-    #
+    # Deallocate memory reallocated by inchi dll.
     # Note the trick with None (Python's NULL) assignment to pointers
-    # which have not been actually allocated (respective strings are zero-length)
+    # which have not been allocated (respective strings are zero-length)
     # within C library libinchi.
     # We passed Python buffer_strings casted to char *, not real pointers
     # so they could not be initialized with NULL (as they would be in C).
     # Therefore, if say szLog or szMessage were actually not used in libinchi.dll
     # and were not allocated, they should be massaged to become NULL at the end,
-    # in order to avoid unnecessary/illegal freeing upon call in freeinchi().
-    if not standard_inchi:
+    # in order to avoid unnecessary/illegal freeing upon call in FreeINCHI().
+    if not inchi:
         c_inchi_output.szInChI = None
-    if not standard_log:
+    if not log:
         c_inchi_output.szLog = None
-    if not standard_message:
+    if not message:
         c_inchi_output.szMessage = None
-    if not standard_aux:
+    if not aux:
         c_inchi_output.szAuxInfo = None
 
     inchi_lib.FreeINCHI(ctypes.byref(c_inchi_output))
 
-    return inchi_exit_code, standard_inchi, standard_log, standard_message, standard_aux
+    return exit_code, inchi, log, message, aux
+
+
+def get_inchi_key_from_inchi(inchi_lib: ctypes.CDLL, inchi: str) -> tuple[int, str]:
+    c_inchi = ctypes.c_char_p(inchi.encode("utf-8"))
+
+    szKey = ctypes.create_string_buffer(256)
+    szXtra1 = ctypes.create_string_buffer(256)
+    szXtra2 = ctypes.create_string_buffer(256)
+
+    exit_code: int = inchi_lib.GetINCHIKeyFromINCHI(
+        c_inchi, 0, 0, szKey, szXtra1, szXtra2
+    )
+
+    key: str = ""
+    if exit_code in [0, 11]:
+        # 11: reversal of InChI failed, InChIKey is computed with warning issued
+        key = ctypes.cast(szKey, ctypes.c_char_p).value.decode("utf-8").strip()
+
+    return exit_code, key
