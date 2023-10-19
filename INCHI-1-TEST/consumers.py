f5f76491a2f992cad8a453322b5d0bf344ec46e7
Jan C. Brammer <jan.c.brammer@gmail.com>
2023-10-19 11:44:10 +0000

    Include log and exit-code in consumer result

--- INCHI-1-TEST/consumers.py
+++ INCHI-1-TEST/consumers.py
@@ -1,4 +1,5 @@
 import ctypes
+import json
 from typing import Callable
 from sdf_pipeline import drivers
 from .inchi_api import make_inchi_from_molfile_text, get_inchi_key_from_inchi
@@ -8,7 +9,7 @@ from .config import INCHI_API_PARAMETERS
 def regression_consumer(
     molfile: str, get_molfile_id: Callable, inchi_lib: ctypes.CDLL
 ) -> drivers.ConsumerResult:
-    _, inchi_string, _, message, aux_info = make_inchi_from_molfile_text(
+    exit_code, inchi_string, log, message, aux_info = make_inchi_from_molfile_text(
         inchi_lib, molfile, INCHI_API_PARAMETERS
     )
     _, inchi_key = get_inchi_key_from_inchi(inchi_lib, inchi_string)
@@ -16,5 +17,14 @@ def regression_consumer(
     return drivers.ConsumerResult(
         get_molfile_id(molfile),
         f"consumer: regression; parameters: {INCHI_API_PARAMETERS}",
-        f"<inchi>: {inchi_string}\t<aux>: {aux_info}\t<key>: {inchi_key}\t<message>: {message}",
+        json.dumps(
+            {
+                "inchi": inchi_string,
+                "key": inchi_key,
+                "aux": aux_info,
+                "log": log,
+                "message": message,
+                "exit": exit_code,
+            }
+        ),
     )
