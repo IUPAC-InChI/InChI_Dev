0efdaa32dcdcdbae8a24daa32447bcaf371e9a4b
gblanke02 <83247497+gblanke02@users.noreply.github.com>
2023-11-19 22:49:55 +0100

    Delete batch-build-vc14 directory
    Delete all Mecurial braches from the release version

--- batch-build-vc14/_rebuild_inchi-1.cmd
+++
@@ -1,9 +0,0 @@
-SET ACT=/Rebuild
-SET NAM=inchi-1
-SET SLN=I:\Dropbox\work\INCHI\devel\INCHI-DEV\INCHI-1-SRC\INCHI_EXE\inchi-1\vc14\inchi-1.sln
-SET BLD= C:\PROGRA~2\MICROS~1.0\COMMON7\IDE\devenv.com
-del rebuild-%NAM%.log
-%BLD% %SLN% /Rebuild "Debug|Win32" /Out rebuild-%NAM%.log
-%BLD% %SLN% /Rebuild "Debug|x64" /Out rebuild-%NAM%.log
-%BLD% %SLN% /Rebuild "Release|Win32" /Out rebuild-%NAM%.log
-%BLD% %SLN% /Rebuild "Release|x64" /Out rebuild-%NAM%.log
--- batch-build-vc14/_rebuild_libinchi.cmd
+++
@@ -1,9 +0,0 @@
-SET ACT=/Rebuild
-SET NAM=libinchi
-SET SLN=I:\Dropbox\work\INCHI\devel\INCHI-DEV\INCHI-1-SRC\INCHI_API\libinchi\vc14\libinchi.vcxproj
-SET BLD= C:\PROGRA~2\MICROS~1.0\COMMON7\IDE\devenv.com
-del rebuild-%NAM%.log
-%BLD% %SLN% %ACT% "Debug|Win32" /Out rebuild-%NAM%.log
-%BLD% %SLN% %ACT% "Debug|x64" /Out rebuild-%NAM%.log
-%BLD% %SLN% %ACT% "Release|Win32" /Out rebuild-%NAM%.log
-%BLD% %SLN% %ACT% "Release|x64" /Out rebuild-%NAM%.log
